281f3700aa7cb25a48621cb00ddf6592702fa169
Tonya-Kot <tonya.kiyanova.04@mail.ru>
2025-06-22 01:20:50 +0300

    Update shopping_cart.py

--- src/shopping_cart.py
+++ src/shopping_cart.py
@@ -6,3 +6,22 @@ def calculate_total_price(items):
     """
     total = sum(item['price'] for item in items)
     return total
+class ShoppingCart:
+    def __init__(self):
+        self.items = []
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def clear(self):
+        self.items = []
+
+def process_payment(cart, amount):
+    """
+    Имитация обработки платежа.
+    Если сумма положительна, платеж успешен.
+    """
+    if amount > 0:
+        cart.clear()
+        return True
+    return False
